e1691536b98a7566608117fa209fa2a5acb5115e
Kavya Aerram <kavyaa@ispace.com>
2020-01-25 03:05:59 -0800

    deployment-ai-4

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 app.use('/problem',function(){
   throw new Error('something is wrong!');
 });
@@ -36,6 +37,7 @@ app.use('/problem',function(){
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
 
 
 // error handler
